8c777cdf4a3c0d142705e96bc8cc68bca8d01118
Chad Brewbaker <crb002@gmail.com>
2017-07-12 13:38:21 -0500

    seeing what files are building

--- vectorAdd/buildLinux.sh
+++ vectorAdd/buildLinux.sh
@@ -3,6 +3,9 @@ clang-3.9 -shared -o addVecISPC.so addVecISPC.o -O3
 clang-3.9 -shared -o addVec.so addVec.c -O3
 clang-3.9 -shared -o addVec0.so addVec.c -O0
 clang-3.9 addVecDriver.c -Wall -O3 -o addVecDriver -ldl
+echo "ls -la"
+ls -la
+
 echo "C -O3 addVec"
 time ./addVecDriver
 echo "C -O0 addVec"
